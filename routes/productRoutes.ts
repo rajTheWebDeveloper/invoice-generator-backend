dfed544207230802434077c29b6fa2ad694ec354
Muni Raj <webdevmaster.raj@gmail.com>
2024-05-31 12:39:26 +0530

    Completed bakend

--- routes/productRoutes.ts
+++ routes/productRoutes.ts
@@ -5,10 +5,9 @@ import validateToken from '../middleware/validateToken'
 
 let router=express.Router()
 
-
 router.post('/product/add',validateToken,addProduct)
 router.get('/products/get',getProducts)
 
-
+console.log("update")
 
 export default router
